2049647c6b7fabdaf5a13db11679d07a7a9ba839
Lara Castelucci <laracastelucci21@gmail.com>
2025-08-27 21:45:56 -0300

    Banco

--- Eclipse-Workspace/projeto_banco_de_dados/src/br/com/senaisp/bauru/lara/teste/ConectorBancoDadosTeste.java
+++ Eclipse-Workspace/projeto_banco_de_dados/src/br/com/senaisp/bauru/lara/teste/ConectorBancoDadosTeste.java
@@ -1,10 +1,12 @@
 package br.com.senaisp.bauru.lara.teste;
 
+import java.sql.SQLException;
+
 import br.com.senaisp.bauru.lara.classes.ConectorBancoDados;
 
 public class ConectorBancoDadosTeste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		ConectorBancoDados conn = ConectorBancoDados.getInstancia();
 		System.out.println(conn.getConnStr());	
 		
